d9102dcc50c0f2bdd77425fc97678fb342dd56fc
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-02-07 12:11:32 -0700

    final version

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-alert('hello')
 const players = {
-    "-1": 'Teal',
-    "1": 'Red',
-    "null": 'green',
+    "-1": 'O',
+    "1": 'X',
+    '' : '',
 }
+const x = "1"
+console.log(players[x])
 const winningCombos = [
     [0,1,2],
     [3,4,5],
@@ -19,12 +20,12 @@ let board;
 let turn;
 let winner;
 
-let move = function(e ){
+let move = function(e) {
     var idx = (e.target.id.replace('cell', ''));
     if (board[idx] || winner) return;
     board[idx] = turn;
     turn *= -1;
-    winner = getWinner();
+    winner = checkWinner();
     render();
 
     }
@@ -41,38 +42,39 @@ document.querySelector('button').addEventListener('click', init);
 
 init();
 function init() {
-    board = [null,null,null,null,null,null,null,null,null];
+    board = ['','','','','','','','',''];
     
     turn = 1;
-    winner = null;
+    winner = '';
 }
 render();
 
 function render() {
     board.forEach(function(cell, idx) {
-         box[idx].style.background = players[cell];
-    
-        });
-        if (winner === 'tie') {
-            whosTurn.textContent = 'its a tie!'
-        } else if (winner) {
+         box[idx].textContent = players[cell];
+    });
+    if (winner === '') {
+        whosTurn.textContent = `${players[turn]}'s turn`;
+    } else if (winner === 'tie') {
+        whosTurn.textContent = `it's a tie`;
+    } else {
+        if (winner === 1) {
             whosTurn.textContent = `${players[winner]} won the game`;
         } else {
-            whosTurn.textContent = `${players[turn]}'s turn`;
+            whosTurn.textContent = `${players[winner]} won the game`;
         }
     }
-    function getWinner() {
-for (var i = 0; i < winningCombos.length; i++) {
-        //if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
-         if (Math.abs(board[0] + board[1] + board[2]) === 3) return board[0];
-         if (Math.abs(board[3] + board[4] + board[5]) === 3) return board[3];
-         if (Math.abs(board[6] + board[7] + board[8]) === 3) return board[6];
-         if (Math.abs(board[0] + board[3] + board[6]) === 3) return board[0];
-         if (Math.abs(board[1] + board[4] + board[7]) === 3) return board[1];
-         if (Math.abs(board[2] + board[5] + board[8]) === 3) return board[2];
-         if (Math.abs(board[0] + board[4] + board[8]) === 3) return board[0];
-         if (Math.abs(board[2] + board[4] + board[6]) === 3) return board[2];
-         if (board.includes(null)) return null;
-         return 'tie';
-    }
+}
+
+function checkWinner() {
+    if (Math.abs(board[0] + board[1] + board[2]) === 3) return board[0];
+    if (Math.abs(board[3] + board[4] + board[5]) === 3) return board[3];
+    if (Math.abs(board[6] + board[7] + board[8]) === 3) return board[6];
+    if (Math.abs(board[6] + board[4] + board[2]) === 3) return board[6];
+    if (Math.abs(board[8] + board[4] + board[0]) === 3) return board[8];
+    if (Math.abs(board[2] + board[5] + board[8]) === 3) return board[2];
+    if (Math.abs(board[0] + board[3] + board[6]) === 3) return board[0];
+    if (Math.abs(board[1] + board[4] + board[7]) === 3) return board[1];
+    if (board.includes('')) return '';
+    return 'tie';
 }
